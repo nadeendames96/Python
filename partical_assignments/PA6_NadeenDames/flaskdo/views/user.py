e731504e9861e812a362adff49f61a751769e7d0
Nadeen <nana1196-4-20@hotmail.com>
2020-09-07 10:14:24 +0300

    Update User File

--- partical_assignments/PA6_NadeenDames/flaskdo/views/user.py
+++ partical_assignments/PA6_NadeenDames/flaskdo/views/user.py
@@ -5,6 +5,8 @@ from ..db import get_db
 bp=Blueprint("user",__name__)
 
 @bp.route('/user/info')
+@bp.route('/users')
+
 def user_info():
     db=get_db()
     users=db.execute("SELECT * FROM User WHERE id=?;", (str(session['uid'],))).fetchall()
@@ -13,3 +15,20 @@ def user_info():
 
 
 # Create User Update
+@bp.route('/user/update',methods=['POST','GET'])
+def user_update():
+    if request.method=='GET':
+        return render_template('user/user_update.html')
+
+    else:
+        firstname=request.form['firstname']
+        lastname=request.form['lastname']
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        user_pucture=request.form['user-picture']
+
+        db=get_db()
+        update=db.execute("UPDATE user SET firstname=?,lastname=?,username=?,email=?,password=?,picture=?;",(firstname,lastname,username,email,password,user_pucture))
+        db.commit()
+        return redirect('/users')
