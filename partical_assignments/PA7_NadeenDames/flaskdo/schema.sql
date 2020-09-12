c28e6d31152cd7c1f901b2a952c76061ed44d53a
Nadeen Omar Dames <nadeendames96@users.noreply.github.com>
2020-09-13 02:46:24 +0300

    __init__.py,db.py,schema.sql files

---
+++ partical_assignments/PA7_NadeenDames/flaskdo/schema.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS TaskList;
+DROP TABLE IF EXISTS Task;
+
+-- Create 'User' table
+CREATE TABLE User (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  firstname TEXT,
+  lastname TEXT,
+  email TEXT UNIQUE NOT NULL,
+  picture TEXT,
+  bod TEXT,
+  address TEXT,
+  password TEXT NOT NULL,
+);
+
+-- Create 'TaskList' table
+CREATE TABLE TaskList (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT, 
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+-- Create 'Task' table
+CREATE TABLE Task (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  task_list_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  priority INTEGER NOT NULL,
+  description TEXT,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  due TEXT,
+  tags TEXT,
+  FOREIGN KEY (task_list_id) REFERENCES taskList (id)
+
+);
+
+CREATE TABLE favorite_tasks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  task_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT,
+  FOREIGN KEY (task_id) REFERENCES Task (id)
+
+);
+
+-- INSERT INTO Date (LastModifiedTime) VALUES(DateTime('now'))
